1e626a759a5f1115830eabfd912a490ba0c1f954
SifanStephanie <sifanl@andrew.cmu.edu>
2018-09-14 19:06:39 +0800

    adding the comments

--- main.cpp
+++ main.cpp
@@ -2,18 +2,45 @@
 #include <random>
 using namespace std;
 
+/**
+ * Learning Algorithms - From MIT courses
+ *
+ * Random-selected algorithm gives a array A and finds the i'th
+ * element we wants to find in order.
+ *
+ * This algorithm uses the Partition metho from the Quick-Sort. As in quicksort,
+ * we partition the input array recursively. But unlike quicksort, which recursively
+ * processes both sides of the partition, RANDOMIZED-SELECT works on only one side of the partition.
+ * It is randommized because there is a random-number generator when we do the partition.
+ *
+ * When we assume that the elements are distinct, the expected running time of Randomized-selected
+ * is O(n).
+ */
+
 #define N 10
 
+/*
+ * generate the random number
+ */
 int Random(int p,int q){
     return p+rand()%(q-p+1);
 }
 
+/*
+ * exchange two numbers
+ */
 void exchange(int A[], int i, int j){
     int temp=A[i];
     A[i]=A[j];
     A[j]=temp;
 }
 
+/*
+ * using the random pivot to partition the
+ * array A into to parts. The elements on the left side <= pivot
+ * and the elements on the right side > pivot.
+ * Then return the position of the partition pivot
+ */
 int Partition(int A[], int p, int q){
     int pivot=A[p];
     int j;
@@ -29,12 +56,16 @@ int Partition(int A[], int p, int q){
     return i;
 }
 
+/*
+ * do the partition using a random number
+ */
 int randomPartition(int A[],int p, int q){
     int i=Random(p,q);
     exchange(A,p,i);
 
     return Partition(A,p,q);
 }
+
 
 int randomSelected(int A[],int p,int q,int i){
     if(p==q)
